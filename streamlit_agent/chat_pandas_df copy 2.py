d24aed29d39f1c3d7e4b664d4e697781856ba42c
Fabio <fabio@onchainlabs.com.br>
2023-12-11 17:06:33 -0300

    Mensagem de commit aqui

--- streamlit_agent/chat_pandas_df copy 2.py
+++ streamlit_agent/chat_pandas_df copy 2.py
@@ -1,35 +1,10 @@
-from langchain.agents import create_csv_agent
-from langchain.llms import OpenAI
+from langchain.agents import AgentType
+from langchain.agents import create_pandas_dataframe_agent
 from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
 import streamlit as st
 import pandas as pd
 import os
-import tempfile
-import io
-import re
-
-def clear_submit():
-    """
-    Clear the Submit Button State
-    """
-    st.session_state["submit"] = False
-
-@st.cache_data(ttl="2h")
-def load_data(uploaded_file):
-    try:
-        ext = os.path.splitext(uploaded_file.name)[1][1:].lower()
-    except:
-        ext = uploaded_file.split(".")[-1]
-    if ext in file_formats:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as f:
-            f.write(uploaded_file.getvalue())
-            return f.name
-    else:
-        st.error(f"Unsupported file format: {ext}")
-        return None
-
-st.set_page_config(page_title="LangChain: Chat with pandas DataFrame", page_icon="🦜")
-st.title("🦜 LangChain: Chat with pandas DataFrame")
 
 file_formats = {
     "csv": pd.read_csv,
@@ -38,6 +13,32 @@ file_formats = {
     "xlsm": pd.read_excel,
     "xlsb": pd.read_excel,
 }
+
+
+def clear_submit():
+    """
+    Clear the Submit Button State
+    Returns:
+
+    """
+    st.session_state["submit"] = False
+
+
+@st.cache_data(ttl="2h")
+def load_data(uploaded_file):
+    try:
+        ext = os.path.splitext(uploaded_file.name)[1][1:].lower()
+    except:
+        ext = uploaded_file.split(".")[-1]
+    if ext in file_formats:
+        return file_formats[ext](uploaded_file)
+    else:
+        st.error(f"Unsupported file format: {ext}")
+        return None
+
+
+st.set_page_config(page_title="Chat with Data", page_icon="🦜")
+st.title("Chat with Data")
 
 uploaded_file = st.file_uploader(
     "Upload a Data file",
@@ -50,6 +51,9 @@ if not uploaded_file:
     st.warning(
         "This app uses LangChain's `PythonAstREPLTool` which is vulnerable to arbitrary code execution. Please use caution in deploying and sharing this app."
     )
+
+if uploaded_file:
+    df = load_data(uploaded_file)
 
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
@@ -66,13 +70,20 @@ if prompt := st.chat_input(placeholder="What is this data about?"):
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    if uploaded_file:
-        temp_path = load_data(uploaded_file)
-        if temp_path:
-            agent = create_csv_agent(OpenAI(temperature=0, api_key=openai_api_key), temp_path, verbose=True)
+    llm = ChatOpenAI(
+        temperature=0, model="gpt-3.5-turbo-0613", openai_api_key=openai_api_key, streaming=True
+    )
 
-            with st.chat_message("assistant"):
-                st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-                response = agent.run(prompt)
-                st.session_state.messages.append({"role": "assistant", "content": response})
-                st.write(response)
+    pandas_df_agent = create_pandas_dataframe_agent(
+        llm,
+        df,
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        handle_parsing_errors=True,
+    )
+
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = pandas_df_agent.run(st.session_state.messages, callbacks=[st_cb])
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(response)
