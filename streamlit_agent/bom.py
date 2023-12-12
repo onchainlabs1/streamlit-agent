24a76c92f879b39d9cf2455b629a59dfeefa835a
Fabio <fabio@onchainlabs.com.br>
2023-12-12 14:41:14 -0300

    Mensagem de commit aqui

--- streamlit_agent/bom.py
+++ streamlit_agent/bom.py
@@ -1,80 +1,74 @@
-from langchain.agents import create_csv_agent
-from langchain.llms import OpenAI
-from langchain.callbacks import StreamlitCallbackHandler
 import streamlit as st
-import pandas as pd
-import os
-import tempfile
-import io
-import re
+from pathlib import Path
+from langchain.llms.openai import OpenAI
+from langchain.agents import create_sql_agent
+from langchain.sql_database import SQLDatabase
+from langchain.agents.agent_types import AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 
-def clear_submit():
-    """
-    Clear the Submit Button State
-    """
-    st.session_state["submit"] = False
+st.set_page_config(page_title="LangChain: Chat with SQL DB", page_icon="🦜")
+st.title("🦜 LangChain: Chat with SQL DB")
 
-@st.cache_data(ttl="2h")
-def load_data(uploaded_file):
-    try:
-        ext = os.path.splitext(uploaded_file.name)[1][1:].lower()
-    except:
-        ext = uploaded_file.split(".")[-1]
-    if ext in file_formats:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as f:
-            f.write(uploaded_file.getvalue())
-            return f.name
-    else:
-        st.error(f"Unsupported file format: {ext}")
-        return None
+# User inputs
+radio_opt = ["Use sample database - Chinook.db", "Connect to your SQL database"]
+selected_opt = st.sidebar.radio(label="Choose suitable option", options=radio_opt)
+if radio_opt.index(selected_opt) == 1:
+    db_uri = st.sidebar.text_input(
+        label="Database URI", placeholder="mysql://user:pass@hostname:port/db"
+    )
+else:
+    db_filepath = (Path(__file__).parent / "Chinook.db").absolute()
+    db_uri = f"sqlite:////{db_filepath}"
 
-st.set_page_config(page_title="LangChain: Chat with pandas DataFrame", page_icon="🦜")
-st.title("🦜 LangChain: Chat with pandas DataFrame")
-
-file_formats = {
-    "csv": pd.read_csv,
-    "xls": pd.read_excel,
-    "xlsx": pd.read_excel,
-    "xlsm": pd.read_excel,
-    "xlsb": pd.read_excel,
-}
-
-uploaded_file = st.file_uploader(
-    "Upload a Data file",
-    type=list(file_formats.keys()),
-    help="Various File formats are Support",
-    on_change=clear_submit,
+openai_api_key = st.sidebar.text_input(
+    label="OpenAI API Key",
+    type="password",
 )
 
-if not uploaded_file:
-    st.warning(
-        "This app uses LangChain's `PythonAstREPLTool` which is vulnerable to arbitrary code execution. Please use caution in deploying and sharing this app."
-    )
+# Check user inputs
+if not db_uri:
+    st.info("Please enter database URI to connect to your database.")
+    st.stop()
 
-openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
-if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+    st.stop()
+
+# Setup agent
+llm = OpenAI(openai_api_key=openai_api_key, temperature=0, streaming=True)
+
+
+@st.cache_resource(ttl="2h")
+def configure_db(db_uri):
+    return SQLDatabase.from_uri(database_uri=db_uri)
+
+
+db = configure_db(db_uri)
+
+toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+agent = create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="What is this data about?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
+user_query = st.chat_input(placeholder="Ask me anything!")
 
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+if user_query:
+    st.session_state.messages.append({"role": "user", "content": user_query})
+    st.chat_message("user").write(user_query)
 
-    if uploaded_file:
-        temp_path = load_data(uploaded_file)
-        if temp_path:
-            agent = create_csv_agent(OpenAI(temperature=0, openai_api_key=openai_api_key), temp_path, verbose=True)
-
-
-
-            with st.chat_message("assistant"):
-                st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-                response = agent.run(prompt)
-                st.session_state.messages.append({"role": "assistant", "content": response})
-                st.write(response)
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container())
+        response = agent.run(user_query, callbacks=[st_cb])
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(response)
--- streamlit_agent/chat_pandas_df.py
+++ streamlit_agent/chat_pandas_df.py
@@ -1,54 +1,73 @@
 import streamlit as st
-from langchain.agents import create_csv_agent
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from langchain.llms import OpenAI
-import tempfile
+from langchain.agents import create_csv_agent
 import os
 
-# Função para carregar dados do arquivo CSV pré-carregado
-def load_data(file_path):
-    with open(file_path, "rb") as f:
-        return f.read()
+# Carregar os dados do CSV
+@st.cache
+def load_data():
+    # Substitua pelo caminho correto do seu arquivo CSV
+    return pd.read_csv('streamlit_agent/binder-data.csv')
 
-# Função para salvar um arquivo CSV temporário
-def save_temporary_csv(file_content):
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as f:
-        f.write(file_content)
-        return f.name
+# Funções para criar e exibir gráficos
+def plot_results_by_type(df):
+    result_counts = df['Tipo de resultado'].value_counts()
+    plt.figure(figsize=(10, 6))
+    result_counts.plot(kind='bar', color='skyblue')
+    plt.title('Resultados por Tipo de Resultado')
+    plt.xlabel('Tipo de Resultado')
+    plt.ylabel('Frequência')
+    st.pyplot()
 
-# Caminho para o arquivo CSV pré-carregado
-DEFAULT_CSV_PATH = "streamlit_agent/binder-data.csv"
+def plot_clicks_over_time(df):
+    df['Início dos relatórios'] = pd.to_datetime(df['Início dos relatórios'])
+    clicks_over_time = df.groupby('Início dos relatórios')['Cliques no link'].sum()
+    plt.figure(figsize=(10, 6))
+    plt.plot(clicks_over_time.index, clicks_over_time.values, marker='o', color='orange')
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+    plt.gcf().autofmt_xdate()
+    plt.title('Cliques no Link ao Longo do Tempo')
+    plt.xlabel('Data')
+    plt.ylabel('Total de Cliques no Link')
+    st.pyplot()
 
 # Configuração inicial do Streamlit
-st.set_page_config(page_title="Campaign Wizard - Binder", page_icon="🦜")
-st.title("Campaign Wizard - Binder")
+st.set_page_config(page_title="LangChain: Chat with Data", page_icon="🦜")
+st.title("🦜 LangChain: Chat with Data")
 
-# Carregando o arquivo CSV pré-carregado
-uploaded_file_content = load_data(DEFAULT_CSV_PATH)
-temp_path = save_temporary_csv(uploaded_file_content)
-
-# Obtendo a chave da API do OpenAI da variável de ambiente
+# Configuração do agente OpenAI
 openai_api_key = os.getenv('OPENAI_API_KEY')
+llm = OpenAI(openai_api_key=openai_api_key, temperature=0)
 
-# Verificando se a chave da API está disponível
-if not openai_api_key:
-    st.error("Chave da API do OpenAI não encontrada. Configure a variável de ambiente OPENAI_API_KEY.")
-    st.stop()
+# Carregar dados
+df = load_data()
 
 # Lógica de chat
-if "messages" not in st.session_state or st.sidebar.button("Limpar Histórico"):
-    st.session_state["messages"] = [{"role": "assistant", "content": "Como posso te ajudar?"}]
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if st.sidebar.button("Clear message history"):
+    st.session_state.messages = []
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="Me pergunte sobre campanhas de marketing"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
+user_query = st.chat_input(placeholder="Ask me anything!")
+if user_query:
+    st.session_state.messages.append({"role": "user", "content": user_query})
 
-    # Criação do agente CSV com a chave da API fornecida
-    agent = create_csv_agent(OpenAI(temperature=0, openai_api_key=openai_api_key), temp_path, verbose=True)
-    
-    with st.chat_message("assistant"):
-        response = agent.run(prompt)
+    # Verificar se o usuário pediu para ver algum dos gráficos
+    if user_query.lower() in ['mostrar gráfico 1', 'quero ver o primeiro gráfico']:
+        plot_results_by_type(df)
+    elif user_query.lower() in ['mostrar gráfico 2', 'quero ver o segundo gráfico']:
+        plot_clicks_over_time(df)
+    else:
+        # Processar pergunta do usuário com o agente OpenAI
+        response = llm.query(user_query)
         st.session_state.messages.append({"role": "assistant", "content": response})
-        st.write(response)
+
+st.sidebar.button("Clear message history")
