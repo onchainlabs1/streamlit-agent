86c9946c2dc951d35604311684a000fea44c0b0e
Fabio <fabio@onchainlabs.com.br>
2023-12-14 10:43:28 -0300

    Descrição geral das mudanças feitas

--- streamlit_agent/chat_pandas_df.py
+++ streamlit_agent/chat_pandas_df.py
@@ -61,7 +61,7 @@ if prompt := st.chat_input(placeholder="Me pergunte sobre campanhas de marketing
 st.sidebar.markdown(
     """
     <div style="position: fixed; bottom: 0; width: auto; text-align: center;">
-        <img src="streamlit_agent/NEW-LOGO-2.png" alt="Logo On-Chain Labs" style="max-height: 100px;">
+        <img src="NEW-LOGO-2.png" alt="Logo On-Chain Labs" style="max-height: 100px;">
         <p style="margin:0;">Powered by On-Chain Labs</p>
     </div>
     """,
